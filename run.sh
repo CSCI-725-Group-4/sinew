de54bc32b6dd5f6d44fe509724c8635b218f2cfd
DevWalrus <70732411+DevWalrus@users.noreply.github.com>
2024-10-28 21:55:53 -0400

    Running everything with local user now

--- run.sh
+++ run.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/zsh
 
-export PG_ROOT=${1-"/usr/local/pgsql"}
+export PG_ROOT=${1-"~/pgsql"}
 if ! (sudo ls $PG_ROOT/data/global > /dev/null); then
 	echo "Initializing postgres"
-	su postgres -c "/usr/local/pgsql/bin/initdb -D /usr/local/pgsql/data/"
+	sudo /usr/local/pgsql/bin/initdb -D ~/pgsql/data/
 fi
 
 echo "Running postgres"
-# su postgres -c "/usr/local/pgsql/bin/postmaster -D /usr/local/pgsql/data"
-su postgres -c "/usr/local/pgsql/bin/postgres -D /usr/local/pgsql/data/"
-su postgres -c "/usr/local/pgsql/bin/createdb test"
+sudo /usr/local/pgsql/bin/postgres -D ~/pgsql/data/
+
+# This needs to be run it a seperate shell window
+# sudo /usr/local/pgsql/bin/createdb test
 
 # # su - postgres
 # /usr/local/pgsql/bin/initdb -D /usr/local/pgsql/data/
