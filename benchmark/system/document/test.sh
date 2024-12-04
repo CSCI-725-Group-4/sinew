b8660c3561d9c35951a5db90aee3cd5740663e46
Danny Gardner <drg5567@rit.edu>
2024-12-03 20:06:16 -0500

    made changes to query test files

--- benchmark/system/document/test.sh
+++ benchmark/system/document/test.sh
@@ -1,20 +1,25 @@
 #!/bin/sh
 
-sed -e 's/"/""/g' < /tmp/dtahara/nb.out | sed -e 's/^{/"{/g' | sed -e 's/,$/"/g' > /tmp/dtahara/nb.esc.out
-rm /tmp/dtahara/nb.out
+export PG_ROOT=/usr/local/pgsql/bin
+export OUT=~/hadapt/results/out/queries
+mkdir -p $OUT
 
-export PG_ROOT=~/pgsql/bin
-export OUT=/tmp/dtahara/out
-mkdir $OUT
-echo "\\timing \\\\ Create table test4(id serial, data document); copy test4(data) from '/tmp/dtahara/nb.esc.out' csv delimiter '|'" | $PG_ROOT/psql test | grep '^Time:' >> $OUT/load
-
-$PG_ROOT/psql test -f upgrade >> $OUT/upgrade
+# Comment these out if rerunning file (table already made + formatted)
+echo "Loading data"
+$PG_ROOT/psql test -f load_data >> $OUT/load_data.out
+# "Upgrading" the data converts it from the json records to the schema
+echo "Upgrading data"
+$PG_ROOT/psql test -f upgrade >> $OUT/upgrade.out
 
 for query in `ls queries`; do
   echo "QUERY $query"
+  mkdir -p $OUT/$query
   for i in 1 2 3 4; do
-    $PG_ROOT/psql test -f queries/$query | grep '^Time:' >> $OUT/$query.out
+    $PG_ROOT/psql test -f queries/$query | grep '^Time:' | sed "s/^$i: /" >> $OUT/$query/$query.out
   done
   echo "==========="
   echo
 done
+
+# Uncomment this line if you want to drop table after
+# $PG_ROOT/psql test -f drop_table
